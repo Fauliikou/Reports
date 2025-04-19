62401bfe387e037d5a088f1d93012b932fe0748b
Fauliikou <156687315+Fauliikou@users.noreply.github.com>
2025-04-19 18:04:31 +0200

    feat: fixed Reportlist

--- src/main/java/org/fauli/reports/commands/ReportListCommand.java
+++ src/main/java/org/fauli/reports/commands/ReportListCommand.java
@@ -31,13 +31,12 @@ public class ReportListCommand implements CommandExecutor {
             player.sendMessage("§7No reports available.");
         } else {
             reportService.getReports().forEach((creator, report) -> {
-
                 String accepted = !report.isAccepted() ? "§cnot accepted" : "§aaccepted";
-
                 String reportInfo = "§7Report from §5" + creator + " §7of type §5" + report.getReportType().getName() + " §7is " + accepted;
+                player.sendMessage(reportInfo);
             });
         }
 
-        return false;
+        return true;
     }
 }
