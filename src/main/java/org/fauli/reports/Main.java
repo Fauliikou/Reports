b64f91876eefb0094d6ee85807f11d0894f6ce49
Fauliikou <156687315+Fauliikou@users.noreply.github.com>
2025-04-19 17:22:09 +0200

    feat: project cleanup

--- src/main/java/org/fauli/reports/Main.java
+++ src/main/java/org/fauli/reports/Main.java
@@ -6,13 +6,6 @@ import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.fauli.reports.commands.ReportChatCommand;
-import org.fauli.reports.commands.ReportCommand;
-import org.fauli.reports.commands.Team.AcceptReportCommand;
-import org.fauli.reports.commands.Team.CloseReportCommand;
-import org.fauli.reports.commands.Team.ViewReportCommand;
-import org.fauli.reports.listener.QuitListener;
-
 
 public final class Main extends JavaPlugin {
 
@@ -24,22 +17,9 @@ public final class Main extends JavaPlugin {
 
         instance = this;
 
-        MiniMessage miniMessage = MiniMessage.miniMessage();
-
-        String text = "<gradient:#3CEF7B:#CDF803>Reports </gradient>" + "<gradient:#EF3C3C:#F8B603>started!</gradient>";
-
-        Component parsed = miniMessage.deserialize(text);
-
-        Bukkit.getConsoleSender().sendMessage(parsed);
-
-        this.getCommand("report").setExecutor(new ReportCommand());
-        this.getCommand("reports").setExecutor(new ViewReportCommand());
-        this.getCommand("reportchat").setExecutor(new ReportChatCommand());
-        this.getCommand("Closereport").setExecutor(new CloseReportCommand());
-        this.getCommand("acceptreport").setExecutor(new AcceptReportCommand());
-
         PluginManager pluginManager = Bukkit.getPluginManager();
-        pluginManager.registerEvents(new QuitListener(), this);
+
+        getSLF4JLogger().info("Reports plugin by Fauli started!");
     }
 
     @Override
--- src/main/java/org/fauli/reports/enums/ReportType.java
+++
@@ -1,7 +0,0 @@
-package org.fauli.reports.enums;
-
-public enum ReportType {
-    QUESTION,
-    BUG,
-    PLAYER
-}
